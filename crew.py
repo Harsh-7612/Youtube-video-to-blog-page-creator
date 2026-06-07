3f71c1141e9da67913193a8fca83ad0045a80ccb
Harsh Kumar <hk13kumar99@gmail.com>
2026-06-07 19:44:41 +0530

    Add files via upload
    Uploaded from local machine

---
+++ crew.py
@@ -0,0 +1,17 @@
+from crewai import Crew, Process
+from agents import blog_researcher, blog_writer
+from task import research_task, writer_task
+
+crew=Crew(
+    agents=[blog_researcher,blog_writer],
+    tasks=[research_task,writer_task],
+    process=Process.sequential,  #Optional: Sequential task execution is default
+    memory=True,
+    cache=True,
+    max_rpm=100,
+    share_crew=True
+)
+
+## start the task execution process with enhanced feedback
+result=crew.kickoff(inputs={'topic':'AI VS ML VS DL VS Data Science'})
+print(result)
---
+++ tools.py
@@ -0,0 +1,4 @@
+from crewai_tools import YoutubeVideoSearchTool
+
+# Initialize the tool with a specific yt channel handle to targeet your search
+yt_tool = YoutubeVideoSearchTool(youtube_channel_handle="@krishnaik06")
