0d1f63eab722018cbde9d698563083a510067740
Harsh Kumar <hk13kumar99@gmail.com>
2026-06-08 13:18:46 +0530

    Delete tools.py

--- tools.py
+++
@@ -1,4 +0,0 @@
-from crewai_tools import YoutubeVideoSearchTool
-
-# Initialize the tool with a specific yt channel handle to targeet your search
-yt_tool = YoutubeVideoSearchTool(youtube_channel_handle=youtube_channel)
